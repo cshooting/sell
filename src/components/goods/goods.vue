d60e9cc653a7cab56d2013f5968e6977a21f263f
cshooting <key874@qq.com>
2018-10-22 19:24:55 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex === index}" @click="selectMenu(index)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>
           {{item.name}}
@@ -12,7 +12,7 @@
     </div>
     <div class="foods-wrapper" ref="foodWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -27,8 +27,11 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">${{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -36,35 +39,83 @@
         </li>
       </ul>
     </div>
+    <shop-cart :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice" :selectFoods="selectFoods"></shop-cart>
   </div>
 </template>
 
 <script>
   import BScorll from 'better-scroll'
+  import ShopCart from "../shopCart/shopCart";
+  import Cartcontrol from "../cartcontrol/cartcontrol";
+
   const ERR_OK = 0;
   export default {
+    components: {
+      Cartcontrol,
+      ShopCart
+    },
     props: {
       seller: Object
     },
     data() {
       return {
-        goods: [],
-        listHeight: []
+        goods: {},
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || this.scrollY >= height1 && this.scrollY < height2) {
+            return i;
+          }
+        }
+        return 0;
+      },
+      selectFoods(){
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if(food.count){
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
       }
     },
     methods: {
-      _initScroll() {
-        this.menuScroll = new BScorll(this.$refs.menuWrapper);
-        this.foodScroll = new BScorll(this.$refs.foodWrapper);
+      selectMenu(index) {
+        let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodScroll.scrollToElement(el,300);
       },
-      // _calculateHeight() {
-      //   let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
-      //   let height = 0;
-      //   this.listHeight.push(height);
-      //   for (let i=0;i<foodList.length;i++){
-      //     let item = foodList[i];
-      //   }
-      // }
+      _initScroll() {
+        this.menuScroll = new BScorll(this.$refs.menuWrapper,{
+          click: true
+        });
+        this.foodScroll = new BScorll(this.$refs.foodWrapper,{
+          click: true,
+          probeType: 3
+        });
+        this.foodScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        //获取每个分类li的高度
+        let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -72,8 +123,10 @@
         res = res.data;
         if (res.errno === ERR_OK) {
           this.goods = res.data;
+          //dom真正发生变化在nextTick函数回调之后，所以在操作原生dom时要在nextTick中操作
           this.$nextTick(function () {
             this._initScroll();
+            this._calculateHeight();
           })
         }
       });
@@ -100,6 +153,12 @@
         width 56px
         line-height 14px
         padding 0 12px
+        &.current
+          position relative
+          z-index 10
+          margin-top -1px
+          background #fff
+          font-weight 700
         .text
           display table-cell
           width 56px
@@ -174,4 +233,8 @@
               text-decoration line-through
               font-size 10px
               color rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position absolute
+            right 0
+            bottom 0
 </style>
--- src/components/headerMsg/headerMsg.vue
+++ src/components/headerMsg/headerMsg.vue
@@ -29,40 +29,42 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail" transition="fade">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
-          </div>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">优惠信息</div>
-            <div class="line"></div>
-          </div>
-          <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="item in seller.supports">
-              <span class="icon" :class="classMap[item.type]"></span>
-              <span class="text">{{item.description}}</span>
-            </li>
-          </ul>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">商家公告</div>
-            <div class="line"></div>
-          </div>
-          <div class="bulletin">
-            <p class="content">{{seller.bulletin}}</p>
-            <p class="content">{{seller.bulletin}}</p>
-            <p class="content">{{seller.bulletin}}</p>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="item in seller.supports">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+              <p class="content">{{seller.bulletin}}</p>
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div class="detail-close" @click="hideDetail">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -216,13 +218,13 @@
       overflow auto
       background rgba(7, 17, 27, 0.8)
       backdrop-filter blur(10px)
-      /*transition all 0.5s
-      &.fade-transition
+      transition all 0.5s linear
+      &.fade-enter-active,&.fade-leave-active
         opacity 1
         background rgba(7, 17, 27, 0.8)
-      &.fade-enter, &.fade-leave
+      &.fade-enter, &.fade-leave-to
         opacity 0
-        background rgba(7, 17, 27, 0)*/
+        background rgba(7, 17, 27, 0)
       .detail-wrapper
         min-height 100%
         width 100%
